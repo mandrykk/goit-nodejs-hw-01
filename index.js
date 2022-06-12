a2e9956524dcbd59b8d6c15659c5516a458ffb6f
mandrykk <mandrykk22@gmail.com>
2022-06-12 20:10:37 +0300

    added files

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
 const contactsOperations = require("./contacts");
 
-const argv = require('yargs').argv;
+const { Command } = require('commander');
+const program = new Command();
+program
+  .option('-a, --action <type>', 'choose action')
+  .option('-i, --id <type>', 'user id')
+  .option('-n, --name <type>', 'user name')
+  .option('-e, --email <type>', 'user email')
+  .option('-p, --phone <type>', 'user phone');
 
-function invokeAction({ action, id, name, email, phone }) {
+program.parse(process.argv);
+
+const argv = program.opts();
+
+async function invokeAction({ action, id, name, email, phone }) {
   switch (action) {
     case 'list':
-      // ...
+      const list = await contactsOperations.listContacts();
+      console.table(list);
       break;
 
     case 'get':
