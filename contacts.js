3d5d4a4087bcac9b0589f208200cb1894a2f408d
mandrykk <mandrykk22@gmail.com>
2022-06-12 19:21:39 +0300

    hw1

---
+++ contacts.js
@@ -0,0 +1,58 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "/db/contacts.json");
+
+async function listContacts() {
+    try {
+        const data = await fs.readFile(contactsPath);
+        return JSON.parse(data);
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+async function getContactById(contactId) {
+    try {
+        const data = await listContacts();
+        const contacts = data.find((contact) => contact.id === contactId);
+        if (!contacts) {
+            return null;
+        }
+        return contacts;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function removeContact(contactId) {
+    try {
+        const data = await listContacts();
+        const newData = data.filter((contact) => contact.id !== contactId);
+        await fs.writeFile(contactsPath, JSON.stringify(newData));
+        const newList = await listContacts();
+        return newList;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function addContact(name, email, phone) {
+    try {
+        const data = await listContacts();
+        const newId = `${data.length + 1}`;
+        data.push({ id: newId, name, email, phone });
+        await fs.writeFile(contactsPath, JSON.stringify(data));
+        const newData = await listContacts();
+        return newData;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+module.exports = {
+    listContacts,
+    getContactById,
+    addContact,
+    removeContact,
+};
---
+++ index.js
@@ -0,0 +1,28 @@
+const contactsOperations = require("./contacts");
+
+const argv = require('yargs').argv;
+
+function invokeAction({ action, id, name, email, phone }) {
+  switch (action) {
+    case 'list':
+      // ...
+      break;
+
+    case 'get':
+      // ... id
+      break;
+
+    case 'add':
+      // ... name email phone
+      break;
+
+    case 'remove':
+      // ... id
+      break;
+
+    default:
+      console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+invokeAction(argv);
